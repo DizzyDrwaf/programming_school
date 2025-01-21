5aac2a85e3c8c89d73fb20e026e6555aa7b1fff9
dizzydrwaf <dizzydrwaf@gmail.com>
2025-01-21 15:13:18 +0100

    black

--- porject_programing/black_jack.py
+++ porject_programing/black_jack.py
@@ -1,37 +1,29 @@
-import random 
+import random
+colors = ['\u2660', '\u2661', '\u2662', '\u2663']
+names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
+play = str(input("Spela black jack?: y/n: "))
 
+while play.lower == "y" or "yes" or "ja" or "j":
 
-deck = [['2 of Hearts', 2], ['3 of Hearts', 3], ['4 of Hearts', 4], ['5 of Hearts', 5], ['6 of Hearts', 6], ['7 of Hearts', 7], ['8 of Hearts', 8], ['9 of Hearts', 9], ['10 of Hearts', 10], ['Jack of Hearts', 10], ['Queen of Hearts', 10], ['King of Hearts', 10], ['Ace of Hearts', (1, 11)], ['2 of Diamonds', 2], ['3 of Diamonds', 3], ['4 of Diamonds', 4], ['5 of Diamonds', 5], ['6 of Diamonds', 6], ['7 of Diamonds', 7], ['8 of Diamonds', 8], ['9 of Diamonds', 9], ['10 of Diamonds', 10], ['Jack of Diamonds', 10], ['Queen of Diamonds', 10], ['King of Diamonds', 10], ['Ace of Diamonds', (1, 11)], ['2 of Clubs', 2], ['3 of Clubs', 3], ['4 of Clubs', 4], ['5 of Clubs', 5], ['6 of Clubs', 6], ['7 of Clubs', 7], ['8 of Clubs', 8], ['9 of Clubs', 9], ['10 of Clubs', 10], ['Jack of Clubs', 10], ['Queen of Clubs', 10], ['King of Clubs', 10], ['Ace of Clubs', (1, 11)], ['2 of Spades', 2], ['3 of Spades', 3], ['4 of Spades', 4], ['5 of Spades', 5], ['6 of Spades', 6], ['7 of Spades', 7], ['8 of Spades', 8], ['9 of Spades', 9], ['10 of Spades', 10], ['Jack of Spades', 10], ['Queen of Spades', 10], ['King of Spades', 10], ['Ace of Spades', (1, 11)]]
+    name_to_value = {}
 
-hand = []
+    value = 2
 
-print(deck)
+    for name in names:
+        name_to_value[name] = value
+        value += 1
 
-handvalue = 0
+    deck = []
+    for color in colors:
+        for value in names:
+            deck.append([value, color])
 
+    random.shuffle(deck)
 
-for kort in deck:
-    handvalue += kort[1]
-    print(handvalue)
+    hand = []
+    for i in range(5):
+        hand.append(deck.pop())
+    
 
-
-
-def deal():
-    card = random.randrange(0, 52)
-
-    hand.append(deck[card])
-    print(hand)
-
-
-   # [['2 of Hearts', 2], ['3 of Hearts', 3], ['4 of Hearts', 4], ['5 of Hearts', 5], ['6 of Hearts', 6], ['7 of Hearts', 7], ['8 of Hearts', 8], ['9 of Hearts', 9], 
-   # ['10 of Hearts', 10], ['Jack of Hearts', 10], ['Queen of Hearts', 10], ['King of Hearts', 10], ['Ace of Hearts', (1, 11)], 
-
-   # ['2 of Diamonds', 2], ['3 of Diamonds', 3], ['4 of Diamonds', 4], ['5 of Diamonds', 5], ['6 of Diamonds', 6], ['7 of Diamonds', 7], ['8 of Diamonds', 8], 
-   # ['9 of Diamonds', 9], ['10 of Diamonds', 10], ['Jack of Diamonds', 10], ['Queen of Diamonds', 10], ['King of Diamonds', 10], ['Ace of Diamonds', (1, 11)], 
-
-   # ['2 of Clubs', 2], ['3 of Clubs', 3], ['4 of Clubs', 4], ['5 of Clubs', 5], ['6 of Clubs', 6], ['7 of Clubs', 7], ['8 of Clubs', 8], ['9 of Clubs', 9], 
-   # ['10 of Clubs', 10], ['Jack of Clubs', 10], ['Queen of Clubs', 10], ['King of Clubs', 10], ['Ace of Clubs', (1, 11)], 
-
-   # ['2 of Spades', 2], ['3 of Spades', 3], ['4 of Spades', 4], ['5 of Spades', 5], ['6 of Spades', 6], ['7 of Spades', 7], ['8 of Spades', 8], ['9 of Spades', 9], 
-   # ['10 of Spades', 10], ['Jack of Spades', 10], ['Queen of Spades', 10], ['King of Spades', 10], ['Ace of Spades', (1, 11)]]
+    play = input("Spela igen?: y/n: ")
