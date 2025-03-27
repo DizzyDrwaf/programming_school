94c498724d7c4cf040ec8444304ca41bddb069b3
DizzyDrwaf <137322658+DizzyDrwaf@users.noreply.github.com>
2025-03-27 08:47:30 +0100

    h

--- pygame/pygame_6.py
+++ pygame/pygame_6.py
@@ -41,13 +41,27 @@ pygame.init()
 # load graphics
 sand_image = pygame.image.load("pygame/img/floor_sand_stone_0.png")
 wall_image = pygame.image.load("pygame/img/brick_brown_0.png")
-#door "img/open_door.png"
-player_image = pygame.image.load("pygame/img/deep_elf_knight_old.png")
-
+chrystal_image = pygame.image.load("pygame/img/crystal_wall_lightmagenta.png")
+door_image = pygame.image.load("pygame/img/open_door.png")
+ogre_image = pygame.image.load("pygame/img/ogre_old.png")
+player_image = pygame.image.load("pygame/img/deep_elf_knight_old.png") 
+# colors 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
 # Add visual elements to the game
 
 wall_size = wall_image.get_width()
 walls = []
+
+door_size = door_image.get_width()
+doors = []
+
+chrystal_size = chrystal_image.get_width()
+chrystals = []
 
 # Create the player
 player = {}
@@ -65,7 +79,7 @@ y = 0
 while len(line) > 1:
     maze_height += 1
     for char in line:
-        if char == 'x' or char == 'd':
+        if char == 'x':
             wall = {}
             wall['x'] = x
             wall['y'] = y
@@ -74,6 +88,18 @@ while len(line) > 1:
         elif char == 'e':
             player['x'] = x
             player['y'] = y
+        elif char == 'c':
+            crystal = {}
+            crystal['x'] = x
+            crystal['y'] = y
+            crystal['image'] = chrystal_image
+            chrystals.append(crystal)
+        elif char == 'd':
+            door = {}
+            door['x'] = x
+            door['y'] = y
+            door['image'] = door_image
+            doors.append(door)
 
         x += wall_size
     x = 0
@@ -81,6 +107,17 @@ while len(line) > 1:
     line = file.readline()
 
 file.close()
+
+# --- player starting direction ---
+
+player_last_direction = "left"
+
+# --- score --- 
+
+score = 0
+font = pygame.font.Font(None, 36)
+
+last_door = "top"
 
 # --- Set the width and height of the screen [width, height]
 size = (maze_width * wall_size, maze_height * wall_size)
@@ -91,55 +128,126 @@ pygame.display.set_caption("Maze Game")
 # --- Game time
 clock = pygame.time.Clock()
 # -------- Main Program Loop -----------
+game_is_pause = False
 is_running = True
 while is_running:
     # --- Event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
-    # --- Game logic should go here
-    # --- Move the player
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        player['x'] -= player['speed']
-        if get_one_colliding_object(player, walls):
-            player['x'] += player['speed']
-    elif keys[pygame.K_RIGHT]:
-        player['x'] += player['speed']
-        if get_one_colliding_object(player, walls):
-            player['x'] -= player['speed']
-    elif keys[pygame.K_UP]:
-        player['y'] -= player['speed']
-        if get_one_colliding_object(player, walls):
-            player['y'] += player['speed']
-    elif keys[pygame.K_DOWN]:
-        player['y'] += player['speed']
-        if get_one_colliding_object(player, walls):
-            player['y'] -= player['speed']
-    else:
-        # snap player to grid
-        player['x'] = round(player['x'] / wall_size) * wall_size
-        player['y'] = round(player['y'] / wall_size) * wall_size
-
-
-
     
+    if game_is_pause == False:
+        # --- Game logic should go here
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            game_is_pause = True
+        
+
+        # --- Move the player
+        if score == 5:
+            game_is_pause = True
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player['x'] -= player['speed']
+            if get_one_colliding_object(player, walls):
+                player['x'] += player['speed']
+            elif get_one_colliding_object(player, chrystals):
+                score += 1
+                chrystals.remove(get_one_colliding_object(player, chrystals))
+            elif get_one_colliding_object(player, doors):
+                if last_door == "top":
+                    player['x'] = wall_size * 10
+                    player['y'] = wall_size * 9
+                    last_door = "bottom"
+                elif last_door == "bottom":
+                    player['x'] = wall_size * 5
+                    player['y'] = wall_size * 5
+                    last_door = "top"
+
+            if (player_last_direction == "right"):
+                player_image = pygame.transform.flip(player_image, True, False)
+                player_last_direction = "left"
+
+        elif keys[pygame.K_RIGHT]:
+            player['x'] += player['speed']
+            if get_one_colliding_object(player, walls):
+                player['x'] -= player['speed']
+            elif get_one_colliding_object(player, chrystals):
+                score += 1
+                chrystals.remove(get_one_colliding_object(player, chrystals))
+
+            
+            if (player_last_direction == "left"):
+                player_image = pygame.transform.flip(player_image, True, False)
+                player_last_direction = "right"
+
+        elif keys[pygame.K_UP]:
+            player['y'] -= player['speed']
+            if get_one_colliding_object(player, walls):
+                player['y'] += player['speed']
+
+            elif get_one_colliding_object(player, chrystals):
+                score += 1
+                chrystals.remove(get_one_colliding_object(player, chrystals))
+            elif get_one_colliding_object(player, doors):
+                if last_door == "top":
+                    player['x'] = wall_size * 10
+                    player['y'] = wall_size * 9
+                    last_door = "bottom"
+                elif last_door == "bottom":
+                    player['x'] = wall_size * 5
+                    player['y'] = wall_size * 5
+                    last_door = "top"
+
+        elif keys[pygame.K_DOWN]:
+            player['y'] += player['speed']
+            if get_one_colliding_object(player, walls):
+                player['y'] -= player['speed']
+
+            elif get_one_colliding_object(player, chrystals):
+                score += 1
+                chrystals.remove(get_one_colliding_object(player, chrystals))
 
 
-    # --- Screen-clearing code goes here
-    # fill widh sand
-    for y in range(0, size[1], wall_size):
-        for x in range(0, size[0], wall_size):
-            screen.blit(sand_image, (x, y))
-    # --- Drawing code should go here
-    for wall in walls:
-        screen.blit(wall_image, (wall['x'], wall['y']))
+        else:
+            # snap player to grid
+            player['x'] = round(player['x'] / wall_size) * wall_size
+            player['y'] = round(player['y'] / wall_size) * wall_size
 
-    screen.blit(player_image, (player['x'], player['y']))
 
-    pygame.display.update()  # or pygame.display.flip()
-    # --- Increase game time
-    clock.tick(60)  # 60 frames per second
 
+
+
+
+        # --- Screen-clearing code goes here
+        # fill widh sand
+        for y in range(0, size[1], wall_size):
+            for x in range(0, size[0], wall_size):
+                screen.blit(sand_image, (x, y))
+        # --- Drawing code should go here
+        for wall in walls:
+            screen.blit(wall_image, (wall['x'], wall['y']))
+
+        for crystal in chrystals:
+            screen.blit(chrystal_image, (crystal['x'], crystal['y']))
+
+        for door in doors:
+            screen.blit(door_image, (door['x'], door['y']))
+
+        screen.blit(player_image, (player['x'], player['y']))
+
+        screen.blit(font.render("Score: " + str(score), True, WHITE), [5, 5])
+
+        if score == 5 and game_is_pause == True:
+            screen.blit(font.render("You Win!!!!", True, WHITE), [(wall_size * 4) + (wall_size * 0.5), (wall_size * 6) - (wall_size * 0.5)])
+            screen.blit(font.render("Press Return to play again", True, WHITE), [(wall_size * 1) + (wall_size * 0.75), (wall_size * 7) - (wall_size * 0.5)])
+
+        pygame.display.update()  # or pygame.display.flip()
+        # --- Increase game time
+        clock.tick(60)  # 60 frames per second
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RETURN]:
+        game_is_pause = False
+        score = 0
 # Clean up when the game exits.
 pygame.quit()
